0bf1a42f910f55e7a371d8e083268b55ef6b9fcd
Jerson Kyle Bonifacio <143814993+JisuKayl@users.noreply.github.com>
2024-11-24 19:56:23 +0800

    first commit

--- app/src/main/java/com/example/ijisu_ai/API.java
+++ app/src/main/java/com/example/ijisu_ai/API.java
@@ -1,7 +1,6 @@
 package com.example.ijisu_ai;
 
-    // This is for testing purposes only. So I'll let it exposed in github commits xD
 public class API {
    // public static String API_URL = "https://api.openai.com/v1/completions";
-   // public static String API = "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";
+   // public static String API = "sk-replace your string api here xD";
 }

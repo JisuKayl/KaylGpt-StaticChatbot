e6ec4256082721fea9f17815eb980055e7579d43
Jerson Kyle Bonifacio <143814993+JisuKayl@users.noreply.github.com>
2024-11-24 20:26:35 +0800

    first commit

---
+++ app/src/main/java/com/example/ijisu_ai/API.java
@@ -0,0 +1,6 @@
+package com.example.ijisu_ai;
+
+public class API {
+   // public static String API_URL = "https://api.openai.com/v1/completions";
+   // public static String API = "sk-replace your string api here xD";
+}

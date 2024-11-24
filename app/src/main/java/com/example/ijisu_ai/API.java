d597cc0ef45da296ff8589d5da4925cb82b2c588
Jerson Kyle Bonifacio <143814993+JisuKayl@users.noreply.github.com>
2024-11-24 20:24:58 +0800

    Exclude API key file from being tracked by Git

--- app/src/main/java/com/example/ijisu_ai/API.java
+++
@@ -1,6 +0,0 @@
-package com.example.ijisu_ai;
-
-public class API {
-   // public static String API_URL = "https://api.openai.com/v1/completions";
-   // public static String API = "sk-replace your string api here xD";
-}
